ad427cf84b78ecd3a7ec4285461af3cbf04d45c8
Aqsha Lathif <axolath@gmail.com>
2022-03-15 17:31:24 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        putih: "#ffffff",
+        hitam: "#00171f",
+        biru1: "#003459",
+        biru2: "#007ea7",
+        biru3: "#00a8e8",
+        biru4: "#c6e2eb",
+      },
+      fontFamily:{
+        fredoka: "'fredoka', sans-serif",
+        mulish: "'mulish', sans-serif"
+      }
+    },
+  },
+  plugins: [],
+}
